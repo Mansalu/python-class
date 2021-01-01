ab7d3c3c3633c0d33fa7f36b88de05b89b46b4b5
Mansalu <mitchell.steinman@utah.edu>
2021-01-01 08:29:24 -0700

    add comments to lecture code

--- Variables/lec.py
+++ Variables/lec.py
@@ -9,15 +9,24 @@ This is a multi-
 # Variables, Types, Basic I/O
 
 # Basic Types in Python
+
+# Integer (int)
 myNumber = 5
 
+# Floating point value (float)
 myFloat = 5.5
 
+# Boolean (bool)
 myBool = True
 
+# String of characters (str)
 myStr = "A string of text"
 
+# This line tells python to print a comma separated sequence of text
 print("My number is", myNumber, "My float is", myFloat, "My Bool is", myBool, "My string is", myStr)
+
+# Input reads the console for use input
+input()
 
 
 
