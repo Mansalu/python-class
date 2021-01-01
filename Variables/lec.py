8de53761c1d8d69e8a9418f9c9feb0256e9faf59
Mansalu <mitchell.steinman@utah.edu>
2020-12-31 17:33:43 -0700

    add lecture 1 on variables types and console IO

---
+++ Variables/lec.py
@@ -0,0 +1,23 @@
+# This is a comment
+
+"""
+This is a multi-
+    line comment
+    Python will ignore anything I put here...
+"""
+
+# Variables, Types, Basic I/O
+
+# Basic Types in Python
+myNumber = 5
+
+myFloat = 5.5
+
+myBool = True
+
+myStr = "A string of text"
+
+print("My number is", myNumber, "My float is", myFloat, "My Bool is", myBool, "My string is", myStr)
+
+
+
---
+++ Variables/main.py
@@ -0,0 +1,28 @@
+# Variables -- Mad Libs Game
+
+print('Hello this is a MadLibs Game')
+
+Name1 = input('Enter a Username: ')
+
+print("Hello " + Name1)
+
+
+while(True):
+    print('The game has begun')
+    MyStory = 'I was walking my little'
+
+    MyStory2 = 'when I saw another dog'
+
+    noun = input('Enter a noun: ')
+
+    Verb = input('Enter a Verb: ')
+
+    print(MyStory,noun,MyStory2,Verb + '.')
+    print('END OF GAME')
+    print('Starting a new game...')
+
+
+
+
+
+
