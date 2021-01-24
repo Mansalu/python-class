d2724b79510a5f6b1ca592b246409ca1201e0ebb
Mansalu <mitchell.steinman@utah.edu>
2021-01-24 12:53:29 -0700

    add new lecture content

--- 011-Files/lec.py
+++ 011-Files/lec.py
@@ -1,6 +1,8 @@
 
-myFile = open("notes.MD", "rt")
-myString = myFile.read()
-listOfWords = myString.split(" ")
-for word in listOfWords:
-    print(word)
+# Write to a file line by line
+sampleFile = open("sampleFile.txt", "xt")
+lineNumber = 0
+while (lineNumber < 10):
+    sampleFile.write("line" + str(lineNumber) + "\n")
+    lineNumber += 1
+sampleFile.close()
