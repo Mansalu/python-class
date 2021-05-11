3c1667fa1060a9d97443c4f0635dc08c14d3e196
Mansalu <mitchell.steinman@utah.edu>
2021-05-10 18:55:27 -0600

    add assignment test file

--- 012-Dictionary/lec.py
+++ 012-Dictionary/lec.py
@@ -6,7 +6,7 @@
 studentMajors = {}
 
 # Let's add some students
-studentMajors.update({"alice" : "Biolody"})
+studentMajors.update({"alice" : "Biology"})
 studentMajors.update({"bob" : "Computer Science"})
 studentMajors.update({"charles" : "Mechanical Engineering"})
 studentMajors.update({"amy" : "English"})
@@ -14,5 +14,14 @@ studentMajors.update({"amy" : "English"})
 # Let's print this information
 for (name,major) in studentMajors.items():
     print(name, "is a", major, "major")
-7
-# Nested Dictionarypo
+
+# Nested Dictionary
+
+studentCourses = {}
+
+for studentName in studentMajors.keys():
+    studentCourses.update({studentName : {"cs101" : "Fundamentals of computer science", 
+                                          "bio101" : "Intro Biology",
+                                          "art200" : "survey of jazz"}})  
+
+print(studentCourses["bob"])
